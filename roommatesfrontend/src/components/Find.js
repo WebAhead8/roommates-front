0eaf5b90327195bad7e5b047c2dd5b0d2bc7f353
khaled ba <badarnakhaled@gmail.com>
2021-03-27 15:37:03 +0300

    hi

---
+++ roommatesfrontend/src/components/Find.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import getAllProfiles from "../fetches/fetchAllProfiles";
+// import fetchAllPosts from "../fetches/fetchAllPosts";
+
+// function FindRoomate() {
+//   return <div>{fetchAllPosts()}</div>;
+// }
+
+// export default FindRoomate;
